ba0b6237365a6e5ed863ec681187924c57d5056e
Ayushi Srivastava <ayushisri@gmail.com>
2019-07-22 14:08:20 -0700

    Script for turtle in level 2

---
+++ Assets/Scripts/TurtleLevel2.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurtleController : MonoBehaviour
+{
+
+    public float turtleSpeed;
+    public float maxPos = 2.15f; 
+
+    Vector3 position; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        position = transform.position; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        position.x += Input.GetAxis("Horizontal") * turtleSpeed * Time.deltaTime;
+
+        position.x = Mathf.Clamp(position.x, -maxPos, maxPos);
+
+        transform.position = position;
+    }
+}

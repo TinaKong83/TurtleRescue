c9370e312fc05c5ac786e61120538601cea52ce7
kongt <tinak2@illinois.edu>
2019-07-23 22:35:42 -0500

    Fixed animation boxes for level 2

--- Assets/Scripts/Dialoguelvl2.cs
+++ Assets/Scripts/Dialoguelvl2.cs
@@ -1,21 +1,68 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Dialoguelvl2 : MonoBehaviour
+public class Dialoguelevel2 : MonoBehaviour
 {
-    private Queue<string> sentences; 
+
+    public Text dialogueText;
+    public Animator animator;
+
+    //first in, first out. Sentences are loaded through this.
+    private Queue<string> sentences;
 
     // Start is called before the first frame update
     void Start()
     {
         sentences = new Queue<string>();
+
     }
 
-    public void StartDialogue (Dialogue dialogue)
+    public void StartDialogue(Dialogue dialogue)
     {
-        Debug.Log("Starting conversation");
+        animator.SetBool("isOpen", true);
+        //Debug.Log("Starting conversation");
+        sentences.Clear();
 
+        //Loop through each sentence in dialogue and add them to your queue
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+
+        //dialogueText.text = sentence;
+        //Debug.Log(sentence);
+    }
+
+    IEnumerator TypeSentence(string sentence)
+    {
+        dialogueText.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            //wait a single frame after each letter
+            yield return null;
+        }
+    }
+
+    public void EndDialogue()
+    {
+        animator.SetBool("isOpen", false);
+        //Debug.Log("End of conversation");
     }
 
 }

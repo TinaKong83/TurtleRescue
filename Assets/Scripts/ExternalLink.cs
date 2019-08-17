86063630d73ee22f15b83c68dc15fc8263ec4204
Ayushi Srivastava <ayushisri@gmail.com>
2019-08-16 17:35:35 -0700

    Changed UI andadded Charity pages

---
+++ Assets/Scripts/ExternalLink.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExternalLink : MonoBehaviour
+{
+    public string LinkName; 
+
+    public void OpenLink()
+    {
+        Application.OpenURL(LinkName);
+    }
+}

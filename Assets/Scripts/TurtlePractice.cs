b923f897fa525b88c18ab48dcf539aa8a3c5922b
Ayushi Srivastava <ayushisri@gmail.com>
2019-08-30 15:09:54 -0700

    Fixed glitch in practice round

--- Assets/Scripts/TurtlePractice.cs
+++ Assets/Scripts/TurtlePractice.cs
@@ -5,24 +5,27 @@ using UnityEngine;
 public class TurtlePractice : MonoBehaviour
 {
     public UIManagerP ui;
+    private bool isColliding; 
      // Start is called before the first frame update
     void Start()
     {
-        
+        isColliding = false; 
     }
 
     // Update is called once per frame
     void Update()
     {
-        float temp = Input.acceleration.x;
+        if (!isColliding)
+        {
+            float temp = Input.acceleration.x;
 
-        transform.Translate(temp, 0f, 0f);
+            transform.Translate(temp, 0f, 0f);
 
-        Vector3 minScreenBounds = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 0));
-        Vector3 maxScreenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+            Vector3 minScreenBounds = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 0));
+            Vector3 maxScreenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
 
-        transform.position = new Vector3(Mathf.Clamp(transform.position.x, minScreenBounds.x + 1, maxScreenBounds.x - 1), Mathf.Clamp(transform.position.y, minScreenBounds.y + 1, maxScreenBounds.y - 1), transform.position.z);
-
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, minScreenBounds.x + 1, maxScreenBounds.x - 1), Mathf.Clamp(transform.position.y, minScreenBounds.y + 1, maxScreenBounds.y - 1), transform.position.z);
+        }
     }
 
     void OnCollisionEnter2D(Collision2D col)
@@ -34,6 +37,7 @@ public class TurtlePractice : MonoBehaviour
         }
         else if (col.gameObject.tag == "Jellyfish")
         {
+            isColliding = true;
             Destroy(col.gameObject);
             ui.GameWon();
         }
